126f58e962bc4e6d061a91eebbb859e49909bb92
yankey <nwolle14@gmail.com>
2026-05-22 04:41:30 +0000

    Ajout d'un controller de navigation

--- app/src/main/java/com/example/dresscode/MainActivity.kt
+++ app/src/main/java/com/example/dresscode/MainActivity.kt
@@ -11,24 +11,49 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.dresscode.ui.home.HomeScreen
+import com.example.dresscode.ui.login.LoginScreen
+import com.example.dresscode.ui.register.RegisterScreen
 import com.example.dresscode.ui.theme.DresscodeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             DresscodeTheme {
+
+                // controller de navigation
+                val navController = rememberNavController()
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    HomeScreen(
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+
+                        composable("home") {
+                            HomeScreen(navController)
+                        }
+
+                        composable("login") {
+                            LoginScreen(navController)
+                        }
+
+                        composable("register") {
+                            RegisterScreen(navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
-
