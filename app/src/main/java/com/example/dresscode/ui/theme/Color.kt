782f18a9f7d353cc4eb4d7e449490d9cb082044f
yankey <nwolle14@gmail.com>
2026-05-27 18:11:11 +0000

    Ajout de nouvelles couleurs dans Color.kt

--- app/src/main/java/com/example/dresscode/ui/theme/Color.kt
+++ app/src/main/java/com/example/dresscode/ui/theme/Color.kt
@@ -12,5 +12,17 @@ val Pink40 = Color(0xFF7D5260)
 
 val Transparent30 = Color(0xD8E7E0E0)
 
+val ColorBackground = Color(0xFFF8F7F4)
 
+val ColorSurface = Color(0xFFFFFFFF)
+
+val ColorBorderDefault = Color(0xFFE2DDD8)
+
+val ColorBorderFocused = Color(0xFF2C2C2C)
+
+val ColorLabel = Color(0xFF9A9590)
+
+val ColorLabelFocused = Color(0xFF2C2C2C)
+
+val ColorText = Color(0xFF1A1A1A)
 
