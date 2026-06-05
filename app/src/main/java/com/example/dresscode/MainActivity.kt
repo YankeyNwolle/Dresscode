53980aa458892cdf54a9c0aa3e165e867c4ef1f4
yankey <nwolle14@gmail.com>
2026-06-05 11:36:51 +0000

    Ajout des routes de navigation

--- app/src/main/java/com/example/dresscode/MainActivity.kt
+++ app/src/main/java/com/example/dresscode/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -40,6 +37,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     ) {
 
+                        // routes
                         composable("home") {
                             HomeScreen(navController)
                         }
@@ -49,7 +47,7 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable("register") {
-                            RegisterScreen(navController)
+                            RegisterScreen(navController,)
                         }
                     }
                 }
